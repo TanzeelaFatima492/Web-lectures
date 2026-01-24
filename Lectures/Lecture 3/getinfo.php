49d89ae792725ac5c2c73f96d1f64094d12ceb97
Tanzeela Fatima <149133736+TanzeelaFatima492@users.noreply.github.com>
2026-01-24 14:27:33 +0500

    Add getinfo.php to display student details
    Create a PHP script to display student information based on GET parameters.

---
+++ Lectures/Lecture 3/getinfo.php
@@ -0,0 +1,10 @@
+<?php
+echo "Welcome ...<br>" ;
+$rn = $_GET["rollno"] ;
+$nm = $_GET["sname"] ;
+$cl = $_GET["sclass"] ;
+echo "$rn<br>";
+//echo $_GET['rollno'];
+echo "Welcome Mr. $nm your Roll No. is $rn and you belongs to $cl class.";
+
+?>
