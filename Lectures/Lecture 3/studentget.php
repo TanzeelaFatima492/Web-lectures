35d3afe827ac61161fdbb282410a24a00360c5c0
Tanzeela Fatima <149133736+TanzeelaFatima492@users.noreply.github.com>
2026-01-24 14:35:04 +0500

    Add studentget.php for student information form

---
+++ Lectures/Lecture 3/studentget.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Get Method</title>
+</head>
+<body>
+	<form action="getinfo.php" method="get">
+		Roll No: <input type="Number" name="rollno"><br>
+		Name: <input type="text" name="sname"><br>
+		Class: <input type="text" name="sclass"><br>
+		<input type="Submit" name="">
+	</form>
+</body>
+</html>
