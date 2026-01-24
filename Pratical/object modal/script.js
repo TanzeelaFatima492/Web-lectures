7c05a222cfe4c20b6c7d86e9801504a797d4a190
Tanzeela Fatima <149133736+TanzeelaFatima492@users.noreply.github.com>
2026-01-24 13:58:15 +0500

    Add files via upload

---
+++ Pratical/object modal/script.js
@@ -0,0 +1,11 @@
+function full(){
+    detail.Fllname.value=detail.Fname.value+" "+detail.Lname.value;
+}
+
+function add(){
+    var a,b,c;
+    a=parseInt(document.Add.num1.value);
+    b=parseInt(document.Add.num2.value);
+    c=a+b;
+    alert("The sum is: "+c);
+}
