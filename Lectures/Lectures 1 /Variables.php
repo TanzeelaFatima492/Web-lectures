d6ac6740d391d752c7314f2e2f6ab92b2b0c8e14
Tanzeela Fatima <149133736+TanzeelaFatima492@users.noreply.github.com>
2026-01-24 14:25:14 +0500

    Add Variables.php to demonstrate variable usage

---
+++ Lectures/Lectures 1 /Variables.php
@@ -0,0 +1,18 @@
+<?php
+echo "<h1>Use of variables</h1><br>";
+
+$num1 = 10    ;
+$num2 = 20.55 ;
+$grade = 'A'  ;
+$sname = "Ali" ;
+echo ($num1);
+echo "<br>";
+echo ($num2);
+echo "<br>";
+echo ($grade);
+echo "<br>";
+echo ($sname);
+echo "<br>";
+
+
+?>
