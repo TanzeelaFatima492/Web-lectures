a67fc52a854807eefe0d16ede393e6546d5888a5
Tanzeela Fatima <149133736+TanzeelaFatima492@users.noreply.github.com>
2026-01-24 14:36:43 +0500

    Add studentpost.php for posting student information

---
+++ Lectures/Lecture 3/studentpost.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Post Method</title>
+</head>
+<body>
+	<form action="postinfo.php" method="Post">
+		Roll No: <input type="Number" name="rollno"><br>
+		Name: <input type="text" name="sname"><br>
+		Class: <input type="text" name="sclass"><br>
+		<input type="Submit" name="">
+	</form>
+</body>
+</html>
