86cc71fe4ae1b879af8c15b8043d02b89cd5f010
Tanzeela Fatima <149133736+TanzeelaFatima492@users.noreply.github.com>
2026-01-24 14:28:37 +0500

    Add postinfo.php to handle student info display

---
+++ Lectures/Lecture 3/postinfo.php
@@ -0,0 +1,10 @@
+<?php
+echo "Welcome ...<br>" ;
+$rn = $_POST["rollno"] ;
+$nm = $_POST["sname"] ;
+$cl = $_POST["sclass"] ;
+echo "$rn<br>";
+
+echo "Welcome Mr. $nm your Roll No. is $rn and you belongs to $cl class.";
+
+?>
