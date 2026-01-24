f7da4b9d941a596a9af721ae0e98fb1f135fc1c2
Tanzeela Fatima <149133736+TanzeelaFatima492@users.noreply.github.com>
2026-01-24 14:26:04 +0500

    Add concatenation.php for variable usage demonstration

---
+++ Lectures/Lecture 2/concatenation.php
@@ -0,0 +1,16 @@
+<?php
+echo "<h1>Use of variables</h1>";
+
+$num1 = 10    ;
+$num2 = 20.55 ;
+$grade = 'A'  ;
+$sname = "Ali" ;
+
+echo "Name is $sname<br>";
+echo 'Name is $sname<br>';
+echo 'Name is '.$sname ." and he belongs to IT4 <br>";
+
+echo '<a href="https://www.google.com/">Google</a><br>';
+echo 'it\'s nice day today';
+
+?>
